f3e626e23fc5162cb002d0aae841f3d875f87e6b
Mickael Bober <mickael.bober@gmail.com>
2020-12-29 10:37:13 +0100

    Add form ArticleType to templates Blog.

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,12 +25,37 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/{id}", name="blog_view")
+     * @Route("/blog/{id}", name="blog_view", requirements={"id"="\d+"})
      */
     public function view(Article $article): Response
     {
         return $this->render('blog/view.html.twig', [
             'article' => $article
         ]);
+    }
+
+    /**
+     * @Route("/blog/new", name="blog_new")
+     * @Route("/blog/{id}/edit", name="blog_edit", requirements={"id"="\d+"})
+     */
+    public function form(Article $article = null, Request $request): Response
+    {
+        if (!$article) {
+            $article = new Article();
+            $article->setCreatedAt(new \DateTime());
+        }
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && !$form->isValid()) {
+            return $this->render('blog/form.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
+        if ($form->isSubmitted() && $form->isValid()) {
+            return new Response('OK');
+        }
+        return $this->render('blog/modal.html.twig', [
+            'form' => $form->createView()
+        ]); 
     }
 }
