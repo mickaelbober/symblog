fcfef2bfd47f53c8cccaf1bd1a81864a8cab2272
Mickael Bober <mickael.bober@gmail.com>
2020-12-30 11:37:22 +0100

    Add encoded password for entity User to database.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -15,7 +16,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *     message = "Cette adresse email existe déja"
  * )
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -102,5 +103,20 @@ class User
         $this->confirmPassword = $confirmPassword;
 
         return $this;
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
     }
 }
