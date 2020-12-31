d3e5cb4d508d4cbd9564c6e0c3c2dd1e122be143
Mickael Bober <mickael.bober@gmail.com>
2020-12-31 10:56:50 +0100

    Add custom form CommentType.

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +13,15 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content')
-            ->add('createdAt')
-            ->add('user')
-            ->add('article')
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu',
+                'label_attr' => [
+                    'class' => 'sr-only'
+                ],
+                'attr' => [
+                    'rows' => '5'
+                ]
+            ])
         ;
     }
 
